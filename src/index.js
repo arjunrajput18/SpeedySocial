ff1756f5b3bdb2bdcbab9ae384e761d0a64eff2d
Arjunsingh Rajput <asr9320003120@gmail.com>
2023-06-08 11:15:26 +0530

    initial setup commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
