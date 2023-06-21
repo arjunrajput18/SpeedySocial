570fac8195bbe883c98614516b0d79c765974ba2
Arjunsingh Rajput <asr9320003120@gmail.com>
2023-06-21 16:55:19 +0530

    fix : some bugs

--- src/Components/SinglePost/SinglePost.js
+++ src/Components/SinglePost/SinglePost.js
@@ -21,15 +21,9 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 export const SinglePost = ({ data }) => {
   const [commentText, setCommentText] = useState();
-
-  const {
-    _id,
-
-    likes: { likeCount, likedBy },
-  } = data;
+ 
   const {
     dataState: { users },
-
     dataDispatch,
     setBtnAddPost,
     btnAddPost,
@@ -37,38 +31,29 @@ export const SinglePost = ({ data }) => {
 
   const socialToken = localStorage.getItem("socialToken");
   const socialUser = JSON.parse(localStorage.getItem("socialUser"));
-  const loggedInuser = users?.find(({ _id }) => _id === socialUser._id);
-  // const newBookmark = bookmarksMatch.bookmarks.some((data) => data === _id);
-console.log(users)
+  const loggedInuser = users?.find(({ username }) => username === socialUser.username);
+// console.log(loggedInuser?.bookmarks)
   const isBookmark = loggedInuser?.bookmarks?.includes(data?._id);
   // console.log(newBookmark);
   const handleLike = () => {
-    // console.log("like");
-    // console.log(data?._id, dataDispatch, socialToken)
-    getlikeData(_id, dataDispatch, socialToken);
+    getlikeData(data._id, dataDispatch, socialToken);
   };
-  // console.log(likedBy.some(({_id})=>_id===data?._id))
   const handledisLike = () => {
-    // console.log("like");
-    getDislikeData(_id, dataDispatch, socialToken);
+
+    getDislikeData(data._id, dataDispatch, socialToken);
   };
-  const btnLike = likedBy?.some((ele) => ele._id === socialUser._id);
+  const btnLike = data?.likes?.likedBy?.some((ele) => ele.username=== socialUser.username);
 
   const navigate = useNavigate();
 
-  // const socialUserId = socialUser._id;
   const handleClick = (userHandler) => {
     navigate(`/profile/${userHandler}`);
   };
-
   const handleBookmark = () => {
-    getBookMark(dataDispatch, socialToken, _id, socialUser.username);
-    // console.log(users);
-    // bookmarkPostHandler(_id, socialToken, dataDispatch, socialUser)
+    getBookMark(dataDispatch, socialToken, data?._id, socialUser.username);
   };
   const handleRemoveBookmark = () => {
-    console.log({_id},"iddddddddddddd")
-    getRemoveBookmarkData(dataDispatch, socialToken, _id, socialUser.username);
+    getRemoveBookmarkData(dataDispatch, socialToken, data?._id, socialUser.username);
   };
 
   const handleProductDetailClick = (postId) => {
@@ -78,7 +63,7 @@ console.log(users)
   const handleAddComment = () => {
     if (commentText) {
       // console.log(_id, commentText, socialToken, dataDispatch);
-      addCommentHandle(_id, commentText, socialToken, dataDispatch);
+      addCommentHandle(data?._id, commentText, socialToken, dataDispatch);
     }
   };
 
@@ -93,11 +78,8 @@ console.log(users)
   };
 
   const handleShare=()=>{
-    // const url=window.location.href
     navigator.clipboard.writeText(`https://speedysocial.netlify.app/post/${data?.id}`)
   }
-
-  // console.log(data, "ll");
   return (
     <div className="singlePost-MainContainer">
       <div className="singlepost-innerContainer">
@@ -150,20 +132,20 @@ console.log(users)
 
         <div className="btn-single-profile">
           {btnLike ? (
-            <span className="btn-like-single-profile">
+            <span className="btn-like-single-profile liked">
               <AiFillHeart onClick={handledisLike} />
-              {likeCount}
+              {data?.likes.likeCount}
             </span>
           ) : (
             <span className="btn-like-single-profile">
               <AiOutlineHeart onClick={handleLike} />
-              {likeCount}
+              {data?.likes.likeCount}
             </span>
           )}
           {/* <BsBookmarkFill onClick={handleRemoveBookmark} /> */}
           <>
             {isBookmark ? (
-              <span className="btn-like-single-profile">
+              <span className="btn-like-single-profile liked">
                 <BsBookmarkFill onClick={handleRemoveBookmark} />
               </span>
             ) : (
